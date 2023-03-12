cf485a93d48607b5e5048bd7f0b17648bb6ba542
Michael <93745624+MB0390231@users.noreply.github.com>
2023-03-12 18:15:17 -0400

    base

--- asset_tracker.py
+++ asset_tracker.py
@@ -3,23 +3,25 @@ from helpers.facebookdatacontainer import FacebookDataContainer
 from helpers.facebookassetgroup import FacebookAssetGroups
 from clickup_python_sdk.api import ClickupClient
 from helpers.clickupjobpool import ClickupJobPool
-from keys import fb_token, app_id, business_id, clickup_token
+from clickup_python_sdk.clickupobjects.folder import Folder
 from helpers import helpers
+import os
 from defaults import (
     DEFAULT_INSIGHTS_FIELDS,
     DEFAULT_INSIGHTS_PARAMS,
-    RGM_CLICKUP_TEAM,
-    RGM_ASSET_TRACKERS_SPACE,
-    ASSET_TRACKER,
+    ASSET_TRACKER_FOLDER_ID,
     DATE_PRESETS,
 )
 
+APP_ID = os.environ["APP_ID"]
+FB_TOKEN = os.environ["FB_TOKEN"]
+BUSINESS_ID = os.environ["BUSINESS_ID"]
+CLICKUP_TOKEN = os.environ["CLICKUP_TOKEN"]
 
-CU = ClickupClient.init(clickup_token)
-FB = FacebookDataContainer(
-    FacebookAdsApi.init(app_id=app_id, access_token=fb_token),
-)
-FB.set_business_ad_accounts(business_id)
+
+FB = FacebookDataContainer(FacebookAdsApi.init(APP_ID=APP_ID, access_token=FB_TOKEN))
+
+FB.set_business_ad_accounts(BUSINESS_ID)
 FB.generate_data_for_batch_date_presets(
     FB.accounts["ACTIVE"], DATE_PRESETS, DEFAULT_INSIGHTS_FIELDS, DEFAULT_INSIGHTS_PARAMS
 )
@@ -29,27 +31,13 @@ COPIES = ASSETS.add_asset_group("ad_name", r"ADC#[a-zA-Z0-9]+", "COPIES")
 CREATIVES = ASSETS.add_asset_group("ad_name", r"VID#[a-zA-Z0-9]+|IMG#[a-zA-Z0-9]+", "CREATIVES")
 
 
-COPY_TRACKER = helpers.get_clickup_list(
-    client=CU,
-    team_name=RGM_CLICKUP_TEAM,
-    space_name=RGM_ASSET_TRACKERS_SPACE,
-    folder_name=ASSET_TRACKER,
-    list_name="Copies",
-)
-CREATIVE_TRACKER = helpers.get_clickup_list(
-    client=CU,
-    team_name=RGM_CLICKUP_TEAM,
-    space_name=RGM_ASSET_TRACKERS_SPACE,
-    folder_name=ASSET_TRACKER,
-    list_name="Creatives",
-)
-LOGS = helpers.get_clickup_list(
-    client=CU,
-    team_name=RGM_CLICKUP_TEAM,
-    space_name=RGM_ASSET_TRACKERS_SPACE,
-    folder_name=ASSET_TRACKER,
-    list_name="Logs",
-).get_tasks()
+CU = ClickupClient.init(CLICKUP_TOKEN)
+ASSET_TRACKER_FOLDER = Folder(id=ASSET_TRACKER_FOLDER_ID)
+
+CLICKUP_ASSETS = ASSET_TRACKER_FOLDER.get_lists()
+COPY_TRACKER = [list for list in CLICKUP_ASSETS if list["name"] == "Copies"][0]
+CREATIVE_TRACKER = [list for list in CLICKUP_ASSETS if list["name"] == "Creatives"][0]
+LOGS = [list for list in CLICKUP_ASSETS if list["name"] == "Logs"][0]
 
 COPY_TRACKER_FIELDS = {field["name"]: field["id"] for field in COPY_TRACKER.get_custom_fields()}
 CREATIVE_TRACKER_FIELDS = {field["name"]: field["id"] for field in CREATIVE_TRACKER.get_custom_fields()}
@@ -66,5 +54,5 @@ for task in CREATIVE_TRACKER_TASKS:
 POOL.process_clickup_jobs()
 
 for dates in FB.date_presets:
-    helpers.write_ads_to_csv(FB.ads_data[dates], f"uploads/{dates}.csv")
-helpers.upload_to_clickup(LOGS)
+    helpers.write_ads_to_csv(FB.insights_data[dates], f"uploads/{dates}.csv")
+helpers.upload_to_clickup(LOGS.get_tasks())
